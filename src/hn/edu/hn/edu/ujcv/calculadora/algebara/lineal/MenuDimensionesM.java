b4c29dc95490ed92a3beda52c4e140d772a0a496
Carlos <cmchamorro99@hotmail.com>
2018-11-08 18:00:07 -0600

    Primera version Menu matrices

--- src/hn/edu/hn/edu/ujcv/calculadora/algebara/lineal/MenuDimensionesM.java
+++ src/hn/edu/hn/edu/ujcv/calculadora/algebara/lineal/MenuDimensionesM.java
@@ -3,6 +3,7 @@ package hn.edu.hn.edu.ujcv.calculadora.algebara.lineal;
 import hn.edu.hn.edu.ujcv.lib.al.Mat2x2;
 import hn.edu.hn.edu.ujcv.lib.al.Mat3x3;
 import hn.edu.hn.edu.ujcv.lib.al.Mat4x4;
+import hn.edu.hn.edu.ujcv.lib.al.VecR2;
 import hn.edu.ujcv.util.LectorTeclado;
 
 public class MenuDimensionesM {
@@ -27,6 +28,23 @@ public class MenuDimensionesM {
             //opcion = lt.leerEntero("Ingrese una opcion", "Ha ingresado una opcion erronea");
             switch (opcion) {
                 case 1: ;
+                    VecR2 col1x = new VecR2(1,0);
+                    VecR2 col1y = new VecR2(0,1);
+                    VecR2 col2x = new VecR2(1,0);
+                    VecR2 col2y = new VecR2(0,1);
+                    Mat2x2 a1 = new Mat2x2(col1x, col1y);
+                    Mat2x2 a2 = new Mat2x2(col1x, col1y);
+                    col1x.setX(lt.leerReal("Ingrese el primer numero de la primera fila de la matriz 1", "Error ha ingresado un caracter no valido"));
+                    col1x.setY(lt.leerReal("Ingrese el segundo numero de la primera fila de la matriz 1", "Error ha ingresado un caracter no valido"));
+                    col1y.setX(lt.leerReal("Ingrese el primer numero  de la segunda fila de la matriz 1", "Error ha ingresado un caracter no valido"));
+                    col1y.setY(lt.leerReal("Ingrese el segundo numero de la segunda fila de la matriz 1", "Error ha ingresado un caracter no valido"));
+                    col2x.setX(lt.leerReal("Ingrese el primer numero de la primera fila de la matriz 2", "Error ha ingresado un caracter no valido"));
+                    col2x.setY(lt.leerReal("Ingrese el segundo numero de la primera fila de la matriz 2", "Error ha ingresado un caracter no valido"));
+                    col2y.setX(lt.leerReal("Ingrese el primer numero  de la segunda fila de la matriz 2", "Error ha ingresado un caracter no valido"));
+                    col2y.setY(lt.leerReal("Ingrese el segundo numero de la segunda fila de la matriz 2", "Error ha ingresado un caracter no valido"));
+                    Mat2x2 res1 = a1.suma(a2);
+                    System.out.println("|" + col1x.getX()+ " "+ col1x.getY()+ "|");
+                    System.out.println("|" + col1y.getX()+ " "+ col1y.getY()+ "|");
                     break;
                 case 2:
                     break;
